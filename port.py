b38e30934dc88f157faad0ca3d326b185cec44f2
Ajith Reddy <43786067+ajithreddygeth@users.noreply.github.com>
2019-08-23 22:58:36 +0530

    Update port.py
    Uncommented the changes

--- port.py
+++ port.py
@@ -4,7 +4,7 @@ for i in range (1,65535):
    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
    result = sock.connect_ex(('30.12.44.123',i))
    if result == 0:
-      print (i,'Portis open')
-   #else:
-      #print ('Port is not open')
+      print (i,'Port is open')
+   else:
+      print ('Port is not open')
       sock.close()
