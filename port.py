659811876eeb94a00f65448a4a8494853564bbc6
Ajith Reddy <43786067+ajithreddygeth@users.noreply.github.com>
2019-08-23 22:57:25 +0530

    Update port.py

--- port.py
+++ port.py
@@ -2,7 +2,7 @@ import socket
 
 for i in range (1,65535):
    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-   result = sock.connect_ex(('3.13.223.201',i))
+   result = sock.connect_ex(('30.12.44.123',i))
    if result == 0:
       print (i,'Portis open')
    #else:
